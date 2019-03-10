500f21481ea787454c9c7db22b05864fcd621d84
Jake Walker <hi@jakew.me>
2019-03-10 22:37:20 +0000

    Initial project

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-vulnlogin
+# VulnLogin
+VulnLogin is a simple vulnerable login page for demonstrating different cyber security tools.
+
+## Current Vulnerabilities
+* **Simple Admin Password** - This login page has a *very* simple passwords. Great for showing off `hydra`!
+* **Too Detailed Errors** - This login page will tell you the username doesn't exist, this means that you can keep testing usernames until you get an error about an incorrect password.
+* **Cookies** - This login page doesn't use sessions, so you can change the username in the cookie to switch users.
+
+## Users
+| Username |
+| -------- |
+| admin |
+| root |
+| guest |
+| michael |
---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const users = require("./users.json");
+
+const app = new express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  if (!users.hasOwnProperty(req.cookies.user)) {
+    console.log("Invalid or no cookie! Redirecting user to login page.");
+    return res.redirect("/login");
+  }
+  
+  console.log("Valid cookie, showing dashboard.");
+  res.render("dashboard", {
+    cookies: req.cookies
+  });
+});
+
+app.get("/logout", (req, res) => {
+  res.clearCookie("user");
+  res.redirect("/login");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login", {
+    user: req.cookies.user
+  });
+});
+
+app.post("/login", (req, res) => {
+  // Sanitize inputs
+  var username = req.body.username.trim();
+  var password = req.body.password;
+
+  console.log(`Login Attempt - ${username}, ${password}`);
+
+  if (!users.hasOwnProperty(username)) {
+    return res.render("login", {
+      error: "Invalid username"
+    });
+  }
+
+  var user = users[username];
+
+  if (user.password != password) {
+    return res.render("login", {
+      error: "Invalid password"
+    });
+  }
+
+  res.cookie("user", username, {
+    maxAge: 900000
+  });
+  return res.redirect("/");
+});
+
+app.use("/", express.static("assets"));
+
+app.listen(3000, () => {
+  console.log("VulnLogin listening on port 3000!");
+});
